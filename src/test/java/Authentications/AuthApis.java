b78c4882ce48fc0ed67fea2a9e08c27e180ad03c
love <lovesoni607@gmail.com>
2020-03-10 14:09:28 +0530

    access token automate

--- src/test/java/Authentications/AuthApis.java
+++ src/test/java/Authentications/AuthApis.java
@@ -2,11 +2,12 @@ package Authentications;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
-import java.util.Map;
-
 import static io.restassured.RestAssured.*;
 import static  org.hamcrest.Matchers.*;
 
@@ -45,7 +46,7 @@ public class AuthApis {
     public void tokenInHeader(){
         baseURI = "https://gorest.co.in";
         given().log().all()
-                .param("first_name","first")
+                .queryParam("first_name","first")
                 .headers(new HashMap<String,String>(){
                     {
                         put("Authorization","Bearer V32Ni9QvrY9oCVjmFl1u7ALvOwimZTKwAzbo");
@@ -56,5 +57,26 @@ public class AuthApis {
                 .then().log().all()
                 .assertThat()
                 .statusCode(200);
+    }
+
+    @Test
+    public void generateOauth2_Using_FormData(){
+       RequestSpecification request =  RestAssured.given()
+                .formParam("client_id","NovApiApp")
+                .formParam("client_secret","24cb2ed6a3ffceb4b10733e845e91963")
+                .formParam("grant_type","client_credentials");
+       Response response = request.post("http://coop.apps.symfonycasts.com/token");
+       System.out.println(response.getStatusCode());
+       System.out.println(response.prettyPrint());
+    }
+
+    @Test
+    public void getResponseFromResponseObject(){
+      RequestSpecification  request =  RestAssured.given()
+                .auth()
+                .preemptive()
+                .oauth2("V32Ni9QvrY9oCVjmFl1u7ALvOwimZTKwAzbo");
+        Response response = request.get("https://gorest.co.in");
+      System.out.println("Print entire JsonPath :"+response.prettyPrint());
     }
 }
