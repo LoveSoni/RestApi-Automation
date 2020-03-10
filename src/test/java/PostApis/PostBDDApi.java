6e6aef096125540db10caba1407ca22096aeac67
love <lovesoni607@gmail.com>
2020-03-10 17:40:23 +0530

    sepepare json body

--- src/test/java/PostApis/PostBDDApi.java
+++ src/test/java/PostApis/PostBDDApi.java
@@ -7,10 +7,12 @@ import io.restassured.specification.RequestSpecification;
 import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 public class PostBDDApi {
 
     @Test
-    public void postHit(){
+    public void postHitWithJSONString(){
         RestAssured.baseURI = "https://gorest.co.in";
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("email","tiim@gmail.com");
@@ -21,6 +23,16 @@ public class PostBDDApi {
         requestSpecification.auth().preemptive().oauth2("V32Ni9QvrY9oCVjmFl1u7ALvOwimZTKwAzbo");
         requestSpecification.contentType(ContentType.JSON);
         requestSpecification.body(jsonObject.toJSONString());
+        Response response = requestSpecification.post("/public-api/users");
+        System.out.println(response.body().asString());
+    }
+
+    @Test void postHitWithJsonFile(){
+        RestAssured.baseURI = "https://gorest.co.in";
+        RequestSpecification requestSpecification = RestAssured.given();
+        requestSpecification.auth().preemptive().oauth2("V32Ni9QvrY9oCVjmFl1u7ALvOwimZTKwAzbo");
+        requestSpecification.contentType(ContentType.JSON);
+        requestSpecification.body(new File(System.getProperty("user.dir")+File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"data"+File.separator+"newUser.json"));
         Response response = requestSpecification.post("/public-api/users");
         System.out.println(response.body().asString());
     }
