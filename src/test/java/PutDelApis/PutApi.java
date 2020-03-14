feffdd3450e6afadb93e85e23967836036a1377d
love <lovesoni607@gmail.com>
2020-03-14 15:59:50 +0530

    put call

--- src/test/java/PutDelApis/PutApi.java
+++ src/test/java/PutDelApis/PutApi.java
@@ -11,10 +11,11 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.testng.annotations.Test;
 
+
 public class PutApi {
     @Test
-    public void putCall(){
-        GoRestUser goRestUser = new GoRestUser("new@gmail.com","firstTime","lastname","male");
+    public void putCall() throws  Exception{
+        GoRestUser goRestUser = new GoRestUser("k2e11011@gmail.com","firstTime","lastname","male");
         String request = null;
         ObjectMapper objectMapper = new ObjectMapper();
         try {
@@ -37,7 +38,19 @@ public class PutApi {
         requestSpecification.contentType(ContentType.JSON);
         requestSpecification.body(requestJson);
         Response response =  requestSpecification.post("/public-api/users");
-        System.out.println("Resonse json{}\n"+response.body().asString());
+        System.out.println("Response json{}\n"+response.body().asString());
+        String userId = response.body().jsonPath().getString("result.id");
+        System.out.println("user id :"+userId);
+        //After creating the data now update the user details using put call
+        goRestUser.setEmail("newTest@gmail.com");
+        String updatedRequest = objectMapper.writeValueAsString(goRestUser);
+        System.out.println("Updated Request json{}\n"+updatedRequest);
+        RequestSpecification putSpecification = RestAssured.given().log().all();
+        putSpecification.contentType(ContentType.JSON);
+        putSpecification.body(updatedRequest);
+        putSpecification.auth().preemptive().oauth2("V32Ni9QvrY9oCVjmFl1u7ALvOwimZTKwAzbo");
+        Response putResponseJson = putSpecification.put("/public-api/users/"+userId);
+        System.out.println(putResponseJson.body().asString());
 
     }
 }
