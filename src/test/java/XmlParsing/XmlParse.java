6086c4ca64eda07709428d5a61483ce1ad3abe99
love <lovesoni607@gmail.com>
2020-03-14 17:44:29 +0530

    Response in the xml format

---
+++ src/test/java/XmlParsing/XmlParse.java
@@ -0,0 +1,25 @@
+package XmlParsing;
+
+import static  io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class XmlParse {
+    @Test
+    public void getXMLResponse(){
+        baseURI = "https://gorest.co.in";
+       Response response =  given().log().all()
+                .queryParam("first_name","first")
+                .header("accept",ContentType.XML)
+                .auth()
+                .oauth2("V32Ni9QvrY9oCVjmFl1u7ALvOwimZTKwAzbo")
+                .when().log().all()
+                .get("/public-api/users");
+       System.out.println("Status code :"+response.getStatusCode());
+       System.out.println("XML Response :"+response.body().asString());
+
+
+    }
+}
