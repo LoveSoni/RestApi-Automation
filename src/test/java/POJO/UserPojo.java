00cac3527679d7ac0437824a842073c69c95240f
love <lovesoni607@gmail.com>
2020-03-14 11:49:50 +0530

    User pojo class

---
+++ src/test/java/POJO/UserPojo.java
@@ -0,0 +1,57 @@
+package POJO;
+
+public class UserPojo {
+    private String firstname;
+    private String lastname;
+    private String totalprice;
+    private String depositpaid;
+    private String additionalneeds;
+
+    public UserPojo(String firstname, String lastname, String totalprice, String depositpaid, String additionalneeds) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.totalprice = totalprice;
+        this.depositpaid = depositpaid;
+        this.additionalneeds = additionalneeds;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getTotalprice() {
+        return totalprice;
+    }
+
+    public void setTotalprice(String totalprice) {
+        this.totalprice = totalprice;
+    }
+
+    public String getDepositpaid() {
+        return depositpaid;
+    }
+
+    public void setDepositpaid(String depositpaid) {
+        this.depositpaid = depositpaid;
+    }
+
+    public String getAdditionalneeds() {
+        return additionalneeds;
+    }
+
+    public void setAdditionalneeds(String additionalneeds) {
+        this.additionalneeds = additionalneeds;
+    }
+}
