b319bd815c4e94c0be3ba53338300bcad64149ed
love <lovesoni607@gmail.com>
2020-03-15 22:01:36 +0530

    Rest Client template

--- src/main/java/Utilities/RestClient.java
+++ src/main/java/Utilities/RestClient.java
@@ -1,8 +1,11 @@
 package Utilities;
 
 
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.apache.commons.lang3.StringUtils;
 import org.json.simple.JSONObject;
-import io.restassured.module.*;
+import sun.misc.Request;
 
 import java.util.Map;
 
@@ -17,30 +20,44 @@ public class RestClient {
     //3. post
     //4. delete
 
-    public void getRequestSpecification(String httpMethod,String baseUrl,String path,Map<String,String> headers){
+    public void sendRequest(Api api)
+    {
+        setBaseUri(api);
+        getRequest(api);
+    }
+
+    public void setBaseUri(Api api){
+        RestAssured.baseURI = api.getBaseUrl();
+    }
+
+    public RequestSpecification getRequest(Api api){
+        String httpMethod = api.getHttpMethod();
+        RequestSpecification request = RestAssured.given().log().all();
         if(httpMethod.equalsIgnoreCase("get")){
 
-        }
-        else if(httpMethod.equalsIgnoreCase("post")){
+        }else if(httpMethod.equalsIgnoreCase("post")){
 
         }
         else if(httpMethod.equalsIgnoreCase("put")){
 
         }
-        else if(httpMethod.equalsIgnoreCase("patch")){
-
-        }
         else if(httpMethod.equalsIgnoreCase("del")){
 
         }
+     return request;
     }
 
-    public void sendRequest(String httpMethod, String baseUrl, String path, Map<String,String> headers, JSONObject jsonObject){
-
-
+    public void setHeaders(Api api,RequestSpecification requestSpecification){
+        if(!api.getHeaders().isEmpty(){
+            requestSpecification.headers(api.getHeaders());
+        }
     }
 
-    public void sendRequest(String httpMethid,String baseUrl,String path,Map<String,String> headers){
-
+    public void setQueryParams(Api api,RequestSpecification requestSpecification){
+        if(!api.getQueryParams().isEmpty()){
+            requestSpecification.queryParams(api.getQueryParams());
+        }
     }
+
+
 }
---
+++ src/test/java/apiTests/test.java
@@ -0,0 +1,11 @@
+package apiTests;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class test {
+    @Test
+    public void test1(){
+
+    }
+}
