b88d7f8f79c16bc77c35593214ad73a4274bd8b8
love <lovesoni607@gmail.com>
2020-03-15 21:34:27 +0530

    Api template with constructor

--- src/main/java/Utilities/Api.java
+++ src/main/java/Utilities/Api.java
@@ -7,11 +7,21 @@ import java.util.Map;
 public class Api {
     private String httpMethod;
 
+    private String baseUrl;
+
     private Map<String,String> queryParams;
 
     private Map<String,String> headers;
 
     private JSONObject requestJson;
+
+    public Api(String httpMethod, String baseUrl, Map<String, String> queryParams, Map<String, String> headers, JSONObject requestJson) {
+        this.httpMethod = httpMethod;
+        this.baseUrl = baseUrl;
+        this.queryParams = queryParams;
+        this.headers = headers;
+        this.requestJson = requestJson;
+    }
 
     public String getHttpMethod() {
         return httpMethod;
@@ -19,6 +29,14 @@ public class Api {
 
     public void setHttpMethod(String httpMethod) {
         this.httpMethod = httpMethod;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
     }
 
     public Map<String, String> getQueryParams() {
