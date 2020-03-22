1abca92d5b80072ffa3ac629cc250a3e4959b749
love <lovesoni607@gmail.com>
2020-03-22 14:53:11 +0530

    handle basic auth api

---
+++ src/test/java/httpClient/BasicAuth.java
@@ -0,0 +1,14 @@
+package httpClient;
+
+import org.testng.annotations.Test;
+
+public class BasicAuth {
+
+    // like in rest assured we can automate the basic auth api like
+    //RequestSpecification request = RestAssured.given()
+    // request.auth().basic("username","password");
+    @Test
+    public void hit(){
+
+    }
+}
