f72f258c42f59de8db214c393d4fc80b21f80f9e
love <lovesoni607@gmail.com>
2020-03-22 13:39:35 +0530

    t :wq
    q
    :wq

--- src/test/java/httpClient/getCall.java
+++ src/test/java/httpClient/getCall.java
@@ -1,11 +1,36 @@
 package httpClient;
 
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.testng.annotations.Test;
+import sun.net.www.http.HttpClient;
+
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class getCall {
     @Test
-    public void hit(){
-
+    public void hit() throws Exception{
+        String baseUrl = "https://gorest.co.in/public-api/users";
+        try {
+            URIBuilder uriBuilder = new URIBuilder(baseUrl);
+            uriBuilder.addParameter("first_name","first");
+            baseUrl = uriBuilder.build().toString();
+        }catch (URISyntaxException e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println("Base url is :"+baseUrl);
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(baseUrl);
+        httpGet.addHeader("Authorization","Bearer V32Ni9QvrY9oCVjmFl1u7ALvOwimZTKwAzbo");
+        HttpResponse response = httpClient.execute(httpGet);
+        System.out.println(EntityUtils.toString(response.getEntity()));
     }
 }
