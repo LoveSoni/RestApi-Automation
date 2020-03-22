4c71ebca54765664dbf487302568e47beb0c25eb
Love <love@Love.local>
2020-03-22 17:05:31 +0530

    rm secret key

--- src/test/java/httpClientOauth/OauthApi.java
+++ src/test/java/httpClientOauth/OauthApi.java
@@ -3,7 +3,6 @@ package httpClientOauth;
 import oauth.signpost.OAuthConsumer;
 import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -13,10 +12,10 @@ import org.testng.annotations.Test;
 public class OauthApi {
     @Test
     public void hit() throws  Exception{
-        String consumerKey = "HGgqWV2t6YnEhhBvuDnAcYlli";
-        String consumerSecret = "LTqWrkWEkcW7FsZO93gq1Z9IfnED8LSY4cpCBAiQGY8AXrLloQ";
-        String accessToken = "220976784-2eQZmLlaEPxeNb3Wywy24dXldUlq1ge29afKA2AR";
-        String accessTokenSecret = "SgXmzqweFoaSilrWLCwYXCYKme4Rk8oXlK4rtE1CUrJN7";
+        String consumerKey = "";
+        String consumerSecret = "";
+        String accessToken = "";
+        String accessTokenSecret = "";
         OAuthConsumer oAuthConsumer = new CommonsHttpOAuthConsumer(consumerKey,consumerSecret);
         oAuthConsumer.setTokenWithSecret(accessToken,accessToken);
         CloseableHttpClient httpClient =HttpClients.createDefault();
